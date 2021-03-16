4d724cac39f4d589b7f080d496a239856fda3063
Misha <73886737+misha244@users.noreply.github.com>
2021-03-16 17:02:47 +0000

    declaring password length variable

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-// Declaring arrays (specify them)
+var generateBtn = Wouldcument.querySelector("#generate");
+// Declaring global variables
 
-lowerCaseArray = [
+const lowercaseArray = [
   "a",
   "b",
   "c",
@@ -30,7 +30,7 @@ lowerCaseArray = [
   "y",
   "z",
 ];
-upperCaseArray = [
+const uppercaseArray = [
   "A",
   "B",
   "C",
@@ -58,8 +58,8 @@ upperCaseArray = [
   "Y",
   "Z",
 ];
-numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-specialCaseArray = [
+const numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const specialcaseArray = [
   "!",
   '"',
   "#",
@@ -94,60 +94,69 @@ specialCaseArray = [
   '"',
 ];
 
-const generatePassword = function () {
-  // Prompt the user and accept the value for password length and store in variable;
+// Prompt the user and accept the value for password length and store in variable;
 
-  // if password length is greater than or equal to 8 and password length is less
-  // const passwordLength = prompt("What is your password length?");
+// if password length is greater than or equal to 8 and password length is less
+// const passwordLength = prompt("What is your password length?");
 
-  // prompt returns a string value eg. 8 is actually "8"
+// prompt returns a string value eg. 8 is actually "8"
 
-  // before if convert string to number Number.parseInt()
+// before if convert string to number Number.parseInt()
 
-  // if TRUE then ask for all 4 requirements using confirm and store each variable
-  // if (passwordLength >= 8 && passwordLength <= 128) {
-  //  console.log("HURRAY - PROCEED");
-  //} else {
-  //  // alert message here
-  //}
+// if TRUE then ask for all 4 requirements using confirm and store each variable
 
-  if (condition) {
-    const isLowercase = confirm("Do you want lowercase?");
-    const isLowercase = confirm("Do you want lowercase?");
-    const isLowercase = confirm("Do you want lowercase?");
-    const isLowercase = confirm("Do you want lowercase?");
-    // edit to the different requirements
-
-    const optionsArray = [["a", "b", "c", "d"][("A", "B", "C", "D")]];
-
-    if (isLowercase) {
-      optionsArray.push(lowerCaseArray);
-    } else if (isUpperCase) {
-      optionsArray.push(upperCaseArray);
-    } else if (isNumbers) {
-      optionsArray.push(numbersArray);
-    } else {
-      optionsArray.push(specialCaseArray);
-    }
-
-    //get random index of options array (0, 1)
-
-    //get the array from the optionsArray[randomIndex]
-
-    //from that array get another random number nbetween 0 and the length of that array
-
-    // get the random character from that array array[3] -> d
-
-    //repeat this loop 8 times
+// Validating password length
+const validatePasswordLength = () => {
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    alert("Thank you. Please proceed");
   } else {
-    alert();
+    alert("Please enter a password length between 8 and 128!");
   }
 };
+
+// Validating character choices
+if (condition) {
+  const isLowercase = confirm(
+    "Would you like your password to include lowercase?"
+  );
+  const isUpperCase = confirm(
+    "Would you like your password to include uppercase?"
+  );
+  const isNumbers = confirm("Would you like your password to include numbers?");
+  const isLowercase = confirm(
+    "Would you like your password to include special characters?"
+  );
+
+  const optionsArray = [["a", "b", "c", "d"][("A", "B", "C", "D")]];
+  const generatePassword = function () {};
+
+  if (isLowercase) {
+    optionsArray.push(lowercaseArray);
+  } else if (isUpperCase) {
+    optionsArray.push(uppercaseArray);
+  } else if (isNumbers) {
+    optionsArray.push(numbersArray);
+  } else {
+    optionsArray.push(specialcaseArray);
+  }
+
+  //get ranWouldm index of options array (0, 1)
+
+  //get the array from the optionsArray[ranWouldmIndex]
+
+  //from that array get another ranWouldm number nbetween 0 and the length of that array
+
+  // get the ranWouldm character from that array array[3] -> d
+
+  //repeat this loop 8 times
+} else {
+  alert();
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = Wouldcument.querySelector("#password");
 
   passwordText.value = password;
 }
